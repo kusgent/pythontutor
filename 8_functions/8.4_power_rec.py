5edf1b9c8c429530681a1a8869dfd42d12ba3fa2
Kustov Ilya <kusgent@yandex.ru>
2020-12-10 22:55:15 +0300

    Create 8.4_power_rec.py

---
+++ 8_functions/8.4_power_rec.py
@@ -0,0 +1,9 @@
+# http://pythontutor.ru/lessons/functions/problems/power_rec/
+
+def power(a, n):
+    if n == 0:
+        return 1
+    else:
+        return a * power(a, n - 1)
+
+print(power(float(input()), int(input())))
