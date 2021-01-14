60fb06f4952fe20a9f92d89bac9c6b6223af877d
Kustov Ilya <kusgent@yandex.ru>
2021-01-14 23:10:12 +0300

    Revision 8.6_fibonacci_rec.py

--- 8_functions/8.6_fibonacci_rec.py
+++ 8_functions/8.6_fibonacci_rec.py
@@ -1,3 +1,5 @@
+# http://pythontutor.ru/lessons/functions/problems/fibonacci_rec/
+
 def fib(n):
     if n == 0:
         return 0
@@ -6,6 +8,5 @@ def fib(n):
     else:
         q = fib(n - 2) + fib(n - 1)
         return q
-
 
 print(fib(int(input())))
