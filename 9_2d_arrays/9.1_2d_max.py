647f4a62bbd3e7ca88977e5919cf73215cff8897
Kustov Ilya <kusgent@yandex.ru>
2021-01-19 22:29:35 +0300

    Create 9.1_2d_max.py

---
+++ 9_2d_arrays/9.1_2d_max.py
@@ -0,0 +1,12 @@
+n, m = [int(i) for i in input().split()]
+a = [[int(j) for j in input().split()] for i in range(n)]
+
+max = a[0][0]
+x, y = 0, 0
+for i in range(n):
+    for j in range(m):
+        if a[i][j] > max:
+            max = a[i][j]
+            x, y = i, j
+
+print(x, y)
