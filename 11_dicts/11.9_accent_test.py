38852c04a5edaeb4138119a8d0fc420b4f5421e1
Kustov Ilya <kusgent@yandex.ru>
2021-07-20 23:16:05 +0300

    Changed 11.9_accent_test.py

--- 11_dicts/11.9_accent_test.py
+++ 11_dicts/11.9_accent_test.py
@@ -1,20 +1,26 @@
 # http://pythontutor.ru/lessons/dicts/problems/accent_test/
 
 n = int(input())
-l = list()
+l1 = list()
+l2 = list()
 for i in range(n):
-    l.append(input())
+    st = input()
+    l1.append(st)
+    l2.append(st.lower())
 
 s = input().split()
 
 q = 0
 for el in s:
     z = 0
-    if el not in l:
+    if el.lower() not in l2:
         for symb in el:
             if symb.isupper() == True:
                 z += 1
         if z != 1:
+            q += 1
+    else:
+        if el not in l1:
             q += 1
 
 print(q)
